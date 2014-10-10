34cc72e6c24f14cda1b61e217c807bc7c0f6a3bc
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-09 23:43:41 -0400

    sidebar updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,32 @@
 $(document).ready( function (){
 
-// API //
 
-var repo_api = 'https://api.github.com/users/DeanGilewicz/repos';
+// USER API //
 
 var user_api = 'https://api.github.com/users/DeanGilewicz';
 
-var starred_api = 'https://api.github.com/users/twhitacre/starred';
 
-var org_api = 'https://api.github.com/users/twhitacre/orgs';
-
-// TEMPLATES //
+// USER TEMPLATES //
 
 var profile_template = $('#profile').html();// PROFILE PIC, NAME, USERNAME //
 var p_template = _.template(profile_template);
 
-var member_since_template = $('#member_since').html();// MEMBER JOINED //
-var ms_template = _.template(member_since_template);
+var contact_template = $('#contact').html();// MEMBER JOINED //
+var con_template = _.template(contact_template);
 
 $.getJSON(user_api).done( function (user_data) {
 
   $('.profile').append(p_template(user_data));
-  $('.member').append(ms_template(user_data));
-
+  $('.member').append(con_template(user_data));
+  
 });
 
 
+var repo_api = 'https://api.github.com/users/DeanGilewicz/repos';
 
+var starred_api = 'https://api.github.com/users/twhitacre/starred';
 
+var org_api = 'https://api.github.com/users/twhitacre/orgs';
 
 
 
