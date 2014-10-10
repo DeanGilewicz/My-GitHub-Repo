6ea526dd193b3c91fff50d4878ed7eaa32d18e85
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-10 10:59:00 -0400

    sidebar orgs section working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,9 +20,6 @@ var c_template = _.template(contact_template);
 var follow_template = $('#follow').html();// FOLLOWERS, STARRED, FOLLOWING //
 var f_template = _.template(follow_template);
 
-var organizations_template = $('#organizations').html();// ORGANIZATIONS AND IMAGES //
-var o_template = _.template(organizations_template);
-
 
 $.getJSON(user_api).done( function (user_data) {
 
@@ -30,16 +27,54 @@ $.getJSON(user_api).done( function (user_data) {
   $('.profile').append(p_template(user_data));
   $('.member').append(c_template(user_data));
   $('.social').append(f_template(user_data));
-  $('.orgs').append(o_template(user_data));
 
 });
 
 
+// ORG API //
+
+var org_api = 'https://api.github.com/users/DeanGilewicz/orgs';
+
+
+// ORG TEMPLATES //
+
+var organizations_template = $('#organizations').html();// ORGANIZATIONS AND IMAGES //
+var o_template = _.template(organizations_template);
+
+
+
+$.getJSON(org_api).done( function (org_data) {
+
+  _.each(org_data, function(x) {
+
+    $('.orgs').append(o_template(x));
+
+  });
+
+  // org_data.forEach( function(x) {
+  //
+  //   $('.orgs').append(o_template(x));
+  //
+  // });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
 var repo_api = 'https://api.github.com/users/DeanGilewicz/repos';
 
-var starred_api = 'https://api.github.com/users/twhitacre/starred';
+var starred_api = 'https://api.github.com/users/DeanGilewicz/starred';
 
-var org_api = 'https://api.github.com/users/twhitacre/orgs';
+
 
 
 
