05e1924ef622c33ce726d7b77b2509ea91d4fc89
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-10 00:57:32 -0400

    sidebar contact and followers updated api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,14 +11,19 @@ var user_api = 'https://api.github.com/users/DeanGilewicz';
 var profile_template = $('#profile').html();// PROFILE PIC, NAME, USERNAME //
 var p_template = _.template(profile_template);
 
-var contact_template = $('#contact').html();// MEMBER JOINED //
-var con_template = _.template(contact_template);
+var contact_template = $('#contact').html();// CONTACT INFO //
+var c_template = _.template(contact_template);
+
+var follow_template = $('#follow').html();// FOLLOWERS, STARRED, FOLLOWING //
+var f_template = _.template(follow_template);
+
 
 $.getJSON(user_api).done( function (user_data) {
 
   $('.profile').append(p_template(user_data));
-  $('.member').append(con_template(user_data));
-  
+  $('.member').append(c_template(user_data));
+  $('.social').append(f_template(user_data));
+
 });
 
 
